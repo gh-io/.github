4ea6ea93d1644bdacb429ee59861a4f6453d2750
Seriki yakub <sandraisom00@gmail.com>
2025-12-29 16:49:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,16 @@ app.whenReady().then(() => {
   const win = new BrowserWindow()
   win.loadURL('https://google.com')
 })
+const { BaseWindow, WebContentsView } = require('electron')
+
+const win = new BaseWindow({ width: 800, height: 400 })
+
+const view1 = new WebContentsView()
+win.contentView.addChildView(view1)
+view1.webContents.loadURL('https://electronjs.org')
+view1.setBounds({ x: 0, y: 0, width: 400, height: 400 })
+
+const view2 = new WebContentsView()
+win.contentView.addChildView(view2)
+view2.webContents.loadURL('https://github.com/electron/electron')
+view2.setBounds({ x: 400, y: 0, width: 400, height: 400 })
