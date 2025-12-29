3d11d7b69db2372bd69136417861cf31d494178f
Seriki yakub <sandraisom00@gmail.com>
2025-12-29 16:48:14 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+app.enableSandbox()
+app.whenReady().then(() => {
+  // any sandbox:false calls are overridden since `app.enableSandbox()` was called.
+  const win = new BrowserWindow()
+  win.loadURL('https://google.com')
+})
