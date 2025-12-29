59c61eac41c06a155ed7ba72aceddd9854472b50
Seriki yakub <sandraisom00@gmail.com>
2025-12-29 23:25:26 +0100

    Create html.ts

---
+++ @proc/html.ts
@@ -0,0 +1,31 @@
+import { html, render } from "@proc/html";
+
+const title = "<Example>";
+
+const someAsyncTask = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve("delayed");
+    }, Math.random() * 1000);
+  });
+};
+
+const template = html`
+  <html>
+    <body>
+      <h1>${title}</h1>
+      <p>
+        ${someAsyncTask().then(
+          text =>
+            html`
+              it's <strong>${text}</strong>!
+            `
+        )}
+      </p>
+    </body>
+  </html>
+`;
+
+render(template).then(rawHTML => {
+  console.log(rawHTML);
+});
