fbc5f61f55a1ced1581e7989445180bbed7cdf83
Seriki yakub <sandraisom00@gmail.com>
2025-12-29 15:50:01 +0100

    Create clone.sh

---
+++ clone.sh
@@ -0,0 +1,8 @@
+ git clone https://github.com/ryanb/railscasts-episodes.git <Rails>
+git clone https://github.com/libgit2/libgit2
+git clone https://github.com/Web4application/kubuverse.git <vite>
+cd kubuverse
+cd kubu
+cd ghcr.io
+cd gh.io
+docker-compose up --build
